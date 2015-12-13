25b38a29490a0a59a92f9c7f49f23807f3df211c
Dennis Aanstoot <dennis@wlan234031.mobiel.utwente.nl>
2015-12-13 18:37:02 +0100

    Waarschijnlijk werkt paddle nu wel

--- js/paddle.js
+++ js/paddle.js
@@ -7,8 +7,6 @@ function Paddle(max_p1, max_p2, width, height, player) {
 
   this.p1 = {x: 0, y: 0};
   this.p2 = {x: 0, y: 0};
-  this.p3 = {x: 0, y: 0};
-  this.p4 = {x: 0, y: 0};
 
   this.middle = {x: 0, y: 0};
   this.middle.x = Math.abs(this.max_p1.x - this.max_p2.x) / 2 + Math.min(this.max_p1.x, this.max_p2.x);
@@ -53,13 +51,9 @@ var distance = function(a,b) {
 Paddle.prototype.set_positions = function (middle) {
   var angle = Math.atan2(this.max_p2.y - this.max_p1.y, this.max_p2.x - this.max_p1.x);
 
-  this.p1.x = middle.x - this.width * Math.sin(angle);
-  this.p1.y = middle.y + this.height * Math.cos(angle);
-  this.p2.x = middle.x + this.width * Math.sin(angle);
-  this.p2.y = middle.y + this.height * Math.cos(angle);
-  this.p3.x = middle.x + this.width * Math.sin(angle);
-  this.p3.y = middle.y - this.height * Math.cos(angle);
-  this.p4.x = middle.x - this.width * Math.sin(angle);
-  this.p4.y = middle.y - this.height * Math.cos(angle);
+  this.p1.x = middle.x - this.width * Math.cos(angle);
+  this.p1.y = middle.y - this.height * Math.sin(angle);
+  this.p2.x = middle.x + this.width * Math.cos(angle);
+  this.p2.y = middle.y + this.height * Math.sin(angle);
 
 };
